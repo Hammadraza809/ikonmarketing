94a6f2fcb1c90a026d194cf2d952c6013fee8a70
Hammad Raza <hammadr809@gmail.com>
2021-07-13 23:45:38 +0500

    Auth corrected

--- src/components/dashboard/pages/chat/Index.js
+++ src/components/dashboard/pages/chat/Index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Content from './Content';
 import SignIn from './SignIn';
-import { useAuthState } from 'react-firebase-hooks';
+import { auth } from '../../pages/common/Config';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function Chat() {
+    const [user] = useAuthState(auth)
     return (
         <div>
-            <SignIn />
-           
+            {user ? <Content /> : <SignIn />}
         </div>
     )
 }
